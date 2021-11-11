21c68a9d654b5fda702f2bc3ca806ad9892c5c31
Ariel <nathan.ariel@ti.ukdw.ac.id>
2021-11-11 20:07:20 +0700

    1_D_71210776

---
+++ cobacobaluaslayangan.py
@@ -0,0 +1,23 @@
+#menghitung luas layangan
+diagonal1 = int(input("masukkan d1:"))
+diagonal2 = int(input("masukkan d2:"))
+
+#proses
+luas = 0.5*(diagonal1*diagonal2)
+
+#print
+print("Luas Layangan", luas)
+
+#menghitung rata-rata uang saku anak
+anakpertama = int(input("Uang saku anak pertama:"))
+anakkedua = int(input("Uang saku anak kedua:"))
+anakketiga = int(input("Uang saku anak ketiga:"))
+
+#proses
+ratarata = (anakpertama+anakkedua+anakketiga)/3
+
+#print
+print("Rata-rata dari adalah", ratarata)
+
+#menghitung catatan belanja
+
